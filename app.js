f2b58fcc3bcf5d5f13e7463b67b6c7724b6b6800
Bartosz Panic <bartosz.panic@gmail.com>
2022-11-01 19:54:08 +0100

    add flag api

--- app.js
+++ app.js
@@ -1,8 +1,68 @@
-const dropList = document.querySelectorAll(".drop-list select");
+const dropList = document.querySelectorAll(".drop-list select"),
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
 
 for (let i = 0; i < dropList.length; i++) {
     for(currency_code in country_list){
-        let optionTag = `<option value="${currency_code}">${currency_code}</option>`;
+
+        let selected;
+        if(i == 0){
+            selected = currency_code == "USD" ? "selected" : "";
+        }else if(i == 1){
+            selected = currency_code == "EUR"? "selected" : "";
+        }
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
+    dropList[i].addEventListener("change", e =>{
+        loadFlag(e.target);
+    })
+}
+
+function loadFlag(element){
+    for(code in country_list){
+        if(code == element.value){
+            let imgTag = element.parentElement.querySelector("img");
+            imgTag.src = `https://countryflagsapi.com/png/${country_list[code].toLowerCase()}`
+        }
+    }
+}
+
+window.addEventListener("load", () =>{
+    getExchangeRate();
+});
+
+getButton.addEventListener("click", e =>{
+    e.preventDefault();
+    getExchangeRate();
+});
+
+const exchangeIcon = document.querySelector(".drop-list .icon");
+exchangeIcon.addEventListener("click", () =>{
+    let tempCode = fromCurrency.value;
+    fromCurrency.value = toCurrency.value;
+    toCurrency.value = tempCode;
+    loadFlag(fromCurrency);
+    loadFlag(toCurrency);
+    getExchangeRate();
+})
+
+function getExchangeRate(){
+    const amount = document.querySelector(".amount input"),
+    exchangeRateTxt = document.querySelector(".exchange-rate");
+    let amountVal = amount.value;
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
+    }
+    let url = `https://api.exchangerate.host/convert?from=${fromCurrency.value}&to=${toCurrency.value}&amount=${amount.value}`;
+    fetch(url).then(response => response.json()).then(result => {
+        let exchangeRate = result.result;
+        exchangeRate = (amountVal * exchangeRate).toFixed(2);
+        let exchangeRateToTxt = document.querySelector(".exchange-rate");
+        exchangeRateToTxt.innerText = `${amountVal} ${fromCurrency.value} = ${exchangeRate} ${toCurrency.value}`
+    }).catch(() => {
+        exchangeRateToTxt.innerText = "Something went wrong"
+    });
 }
